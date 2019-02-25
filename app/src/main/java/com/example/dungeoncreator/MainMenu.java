a18474a6d0b5a70a6b294f28c3f2652b7a65c87b
jorgeleonardolf <jorgeleonardolf@gmail.com>
2019-02-24 21:33:03 -0600

    Added Filters button, Random Select button, Saved Dungeons button

--- app/src/main/java/com/example/dungeoncreator/MainMenu.java
+++ app/src/main/java/com/example/dungeoncreator/MainMenu.java
@@ -1,7 +1,14 @@
 package com.example.dungeoncreator;
 
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
-public class MainMenu extends AppCompatActivity implements View.OnClickListener {
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+    }
 }
